d610358e6b56a8637e80ae11fec823516722a700
TAEJIN KIM <taejin0323@gmail.com>
2017-10-27 22:13:34 +1000

    삽입정렬1

---
+++ HackerRank/Sorting/03.insertionSort_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void insertionSort(vector <int> ar) {
+	int rightmost = ar.back();
+	for (int i = ar.end()-2; i > 0; i--) {
+		if (ar[i] > rightmost) {
+			ar[i + 1] = ar[i];
+		}
+		for (int j = 0; j < ar.size(); j++) {
+			cout << ar[j] << ' ';
+		}
+		cout << endl;
+	}
+}
+
+int main(){
+	vector <int> _ar;
+	int _ar_size;
+	cin >> _ar_size;
+	for(int _ar_i=0; _ar_i<_ar_size; _ar_i++){
+		int _ar_temp;
+		cin >> _ar_temp;
+		_ar.push_back(_ar_temp);
+	}
+
+	insertionSort(_ar);
+	return 0;
+}
